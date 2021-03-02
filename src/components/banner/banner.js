0bbf35a2a687d346c2d9d8d418f7dd7d620ba077
oranje19 <jasonwintery@gmail.com>
2021-03-01 22:28:10 -0500

    Add Banner

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './banner.css';
 
 function Banner() {
     return (
-        <div className="banner">
-            
-        </div>
+        <header 
+            className="banner" 
+            style={{
+                backgroundSize: "cover",
+                backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+                backgroundPosition: "center center"
+        }}>
+            <div className="banner__contents">
+                <h1 className="banner__title">
+                    Movie Name
+                </h1>
+                <div className="banner__buttons">
+                    <button className="banner__button">Play</button>
+                    <button className="banner__button">My List</button>
+                </div>
+                <h1 className="banner__description">
+                    This is a test description
+                </h1>
+            </div>
+            <div className="banner__fadeBottom">
+
+            </div>
+        </header>
     )
 }
 
