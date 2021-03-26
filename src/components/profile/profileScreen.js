2132d77e8120da048049ed02fdcc13ae0cac94b8
oranje19 <jasonwintery@gmail.com>
2021-03-25 23:55:23 -0400

    Add instructions for Stripe info

--- src/components/profile/profileScreen.js
+++ src/components/profile/profileScreen.js
@@ -15,10 +15,34 @@ function ProfileScreen() {
             <div className="profileScreen__body">
                 <h1>Edit Profile</h1>
                 <div className="profileScreen__info">
-                    <img 
-                        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
-                        alt=""
-                    />
+                    <div className="profileScreen__avatar">
+                        <img 
+                            src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+                            alt=""
+                        />
+                        <span className="helpIcon">
+                            <i className="far fa-question-circle fa-3x"></i>
+                            <span class="tooltip">
+                                <p>For DEMO purposes</p>
+                                <br/>
+                                <p>Use the following information:</p>
+                                <br/>
+                                <p>Testing Card Number:</p>
+                                <p>4242 4242 4242 4242</p>
+                                <br/>
+                                <p>Date:</p>
+                                <p>Any future date</p>
+                                <br/>
+                                <p>CVC:</p>
+                                <p>Any 3 digits</p>
+                                <br/>
+                                <p>Address:</p>
+                                <p>Any valid Address</p>
+
+                            </span>
+                        </span>
+                    </div>
+                    
                     <div className="profileScreen__details">
                         <h2>{user.email}</h2>
                         <div className="profileScreen__plans">
